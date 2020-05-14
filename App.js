28712ea9e2af462d47e0b8124b0cdb63a87a3f8d
Umair Anjum <umairanjum82@gmail.com>
2020-05-14 22:29:49 +0500

    Added some properties using Inline and Internal  CSS

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Text, View,Button} from 'react-native'
+import {Text, View,Button,StyleSheet} from 'react-native'
 
 class Myclass extends Component{
 
@@ -15,9 +15,11 @@ myfuction(parametr){
   render(){
     return(
 
-<View>
-<Text>Hello World</Text>
-<Text>{this.state.newTitle}</Text>
+<View style={style.mainView}>
+<Text style={style.headerText}>Hello World</Text>
+<Text style={{...style.otherText,backgroundColor:"pink"}}>{this.state.newTitle}</Text>
+<Text style={style.otherText}>{this.state.newTitle}</Text>
+
 <Button
  title={this.state.mytitle}
  onPress={()=>this.myfuction("my string")}
@@ -26,5 +28,27 @@ myfuction(parametr){
     );
   }
 }
+const style = StyleSheet.create({
 
+mainView:{
+  padding: 10,
+  borderRadius : 20,
+  borderWidth:2 ,
+  margin:20
+},
+
+ headerText:{
+   fontSize:20,
+   color:"red",
+   margin: 10,
+   padding: 10
+ },
+ otherText:{
+   fontSize : 14,
+   color:"red",
+   backgroundColor:"black",
+   padding : 10,
+   margin : 10
+ }
+});
 export default Myclass
